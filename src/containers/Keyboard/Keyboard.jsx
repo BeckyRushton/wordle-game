5c269ff938a3938dc4051914c1007b2b3f98a059
Becky Rushton <beckyrushton30@gmail.com>
2023-06-09 10:34:40 +0100

    removed js data file and did an array in keyboard instead and styled letters

--- src/containers/Keyboard/Keyboard.jsx
+++ src/containers/Keyboard/Keyboard.jsx
@@ -1,9 +1,46 @@
 import React from "react";
 import "./Keyboard.scss";
-// import Qwerty from "../../data/qwerty.js";
 
 const Keyboard = () => {
-  return <div className="keyboard-tiles">Keyboard</div>;
+  const qwertyArr = [
+    "Q",
+    "W",
+    "E",
+    "R",
+    "T",
+    "Y",
+    "U",
+    "I",
+    "O",
+    "P",
+    "A",
+    "S",
+    "D",
+    "F",
+    "G",
+    "H",
+    "J",
+    "K",
+    "L",
+    "Z",
+    "X",
+    "C",
+    "V",
+    "B",
+    "N",
+    "M",
+    "←",
+    "✓",
+  ];
+  return (
+    <div className="keyboard-tiles">
+      {qwertyArr.map((item, index) => (
+        <p className="keyboard-tiles__letter" key={index}>
+          {item}
+        </p>
+      ))}
+    </div>
+  );
   // JSX keyboard, map function
 };
 
